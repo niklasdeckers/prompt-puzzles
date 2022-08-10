8fba47ccad3f086fc8829d00fddbbad7e4d28a1e
Niklas Deckers <30595028+niklasdeckers@users.noreply.github.com>
2022-08-10 10:41:42 +0200

    Add alternative backend URL env

--- util.py
+++ util.py
@@ -7,15 +7,25 @@ import requests
 
 
 def get_dalle_images(prompt, num_images=1):
-    backend_url = os.environ.get('DALLE_BACKEND_URL') + "/dalle"
-    r = requests.post(backend_url, json={"text": prompt, "num_images": num_images})
-    if r.status_code == 200:
-        json = r.json()
-        images = json["generatedImgs"]
-        images = [base64.b64decode(img) for img in images]
-        return images
-    else:
+    urls = []
+    if os.environ.get('DALLE_BACKEND_URLS'):
+        urls += json.loads(os.environ.get('DALLE_BACKEND_URLS'))
+    if os.environ.get('DALLE_BACKEND_URL'):
+        urls.append(os.environ.get('DALLE_BACKEND_URL'))
+
+    r = None
+    for url in urls:
+        backend_url = url + "/dalle"
+        r = requests.post(backend_url, json={"text": prompt, "num_images": num_images})
+        if r.status_code == 200:
+            json = r.json()
+            images = json["generatedImgs"]
+            images = [base64.b64decode(img) for img in images]
+            return images
+    if r:
         raise urllib.error.URLError(f"Error {r.status_code} while fetching DALLE results")
+    else:
+        raise ValueError("No backend URL specified")
 
 
 def get_gpt3_response(prompt):
